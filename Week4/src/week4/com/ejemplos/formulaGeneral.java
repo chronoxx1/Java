bca55f1a46fe495636c8d56527a547b958dfd96d
José Chávez <jose271_@hotmail.com>
2017-10-16 14:40:41 -0500

    Semana 4 Ejercicio 1

---
+++ Week4/src/week4/com/ejemplos/formulaGeneral.java
@@ -0,0 +1,26 @@
+package week4.com.ejemplos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pepe
+ */
+public class formulaGeneral {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Ingrese..");
+
+        double a = read.nextDouble();
+        double b = read.nextDouble();
+        double c = read.nextDouble();
+
+        double x1 = ((-b) + Math.sqrt(Math.pow(b, 2) - (4 * a * c)) / (2 * a));
+        double x2 = ((-b) - Math.sqrt(Math.pow(b, 2) - (4 * a * c)) / (2 * a));
+
+        System.out.println(x1);
+        System.out.println(x2);
+    }
+}
